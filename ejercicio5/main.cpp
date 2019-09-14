d73a5b50da92c22658b2143fcf8c737979d306ce
limartel <40374581+limartel@users.noreply.github.com>
2019-09-13 21:53:29 -0500

    Update main.cpp

--- ejercicio5/main.cpp
+++ ejercicio5/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include "Tipos.h"
+
 using namespace std;
-
-int main() {
-
-entero numero;
-caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+int main(){
+    int numero,conta=0,conti=0;
+    do{
+        cout<<"Ingrese un numero: ";
+        cin>>numero;
+        if (numero%2==0){
+            conta=conta+1;
+        }
+        else{
+            conti=conti+1;
+        }
+    }
+    while(numero!=0);
+    cout<<"La cantidad de numeros pares es: "<<conta-1<<endl;
+    cout<<"La cantidad de numeros impares es: "<<conti<<endl;
 }
