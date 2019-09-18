81213d9e8637147c8b9018245f7ee7673e985661
limartel <40374581+limartel@users.noreply.github.com>
2019-09-18 10:14:05 -0500

    Update main.cpp

--- ejercicio8/main.cpp
+++ ejercicio8/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include "Tipos.h"
 using namespace std;
-
 int main() {
+  int n;
+  cin>>n;
 
-entero numero;
-
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+  if(n>=1 && n<=30){
+    for(int i=0;i<n;i++){
+    for(int k=0;k<(n-1)-i;k++){
+      cout<<" ";
+    }
+    for(int k=0;k<(i*2)+1;k++){
+      cout<<"*";
+      }
+    cout<<endl;
+    }
+  }
 }
